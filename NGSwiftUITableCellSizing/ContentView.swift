c10f99a3bbe96ee5b2893deaa54648272e9bfcd8
Noah Gilmore <noah.w.gilmore@gmail.com>
2020-07-06 06:59:28 -0700

    Initial Commit

---
+++ NGSwiftUITableCellSizing/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  NGSwiftUITableCellSizing
+//
+//  Created by Noah Gilmore on 7/6/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
