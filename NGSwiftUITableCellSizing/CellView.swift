b33f355a8099b4ee900b7d522fc187a00ff6114e
Noah Gilmore <noah.w.gilmore@gmail.com>
2020-07-15 09:35:23 -0700

    Some cleanup

--- NGSwiftUITableCellSizing/CellView.swift
+++ NGSwiftUITableCellSizing/CellView.swift
@@ -18,35 +18,35 @@ struct CellView: View {
                 .frame(width: 2)
             VStack(alignment: .leading, spacing: 4) {
                 Text(self.content)
-                    .lineLimit(1)
+                    .lineLimit(nil)
                 Text(String(repeating: content + " ", count: numberOfRepetitions))
                     .font(.caption)
                     .foregroundColor(Color(.secondaryLabel))
                     .lineLimit(nil)
-                    .layoutPriority(1)
             }
             .padding([.top, .bottom], 10)
             Spacer()
         }
         .padding([.leading, .trailing], 10)
-        .edgesIgnoringSafeArea(.bottom)
-        .fixedSize(horizontal: false, vertical: true)
     }
 }
 
 struct ComplicatedCellView: View {
     let value: Int
-    @State var isColorInverted = false
 
     var body: some View {
         VStack {
             HStack {
                 VStack {
-                    Rectangle().fill(isColorInverted ? Color.red : Color.green).frame(width: 50, height: 50)
-                    Rectangle().fill(isColorInverted ? Color.green : Color.red).frame(width: 50, height: 50)
+                    Rectangle().fill(Color.green)
+                        .frame(width: 50, height: 50)
+                    Rectangle().fill(Color.red)
+                        .frame(width: 50, height: 50)
                     Spacer()
-                }.frame(minHeight: 150).animation(.spring())
-                Rectangle().fill(Color.blue).frame(minWidth: 100)
+                }.frame(minHeight: 100)
+                Rectangle()
+                    .fill(Color.blue)
+                    .frame(minWidth: 100)
                 VStack {
                     Text(String(repeating: "Value ", count: value % 40) + "\(value % 40)")
                         .lineLimit(nil)
@@ -55,9 +55,7 @@ struct ComplicatedCellView: View {
                     Spacer()
                 }
             }
-            Button(action: {
-                self.isColorInverted.toggle()
-            }, label: {
+            Button(action: {}, label: {
                 Text("Tap me!")
             }).padding(.bottom, 5)
         }
@@ -65,7 +63,5 @@ struct ComplicatedCellView: View {
         .padding()
         .frame(minHeight: 0, maxHeight: .infinity)
         .border(Color.purple)
-        .edgesIgnoringSafeArea(.all)
-        .fixedSize(horizontal: false, vertical: true)
     }
 }
