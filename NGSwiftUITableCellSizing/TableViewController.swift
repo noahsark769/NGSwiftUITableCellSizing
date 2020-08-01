aa2e7e5b1005850ced4a2f963606ec3e9c75b66b
Federico Zanetello <zntfdr@gmail.com>
2020-08-01 12:25:24 +0700

    simplify view

--- NGSwiftUITableCellSizing/TableViewController.swift
+++ NGSwiftUITableCellSizing/TableViewController.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 import UIKit
+import SwiftUI
 
 final class TableViewController: UITableViewController {
     override init(style: UITableView.Style) {
         super.init(style: style)
-        self.tableView.register(HostingCell<CellView>.self, forCellReuseIdentifier: "HostingCell<CellView>")
-        self.tableView.register(HostingCell<ComplicatedCellView>.self, forCellReuseIdentifier: "HostingCell<ComplicatedCellView>")
+        self.tableView.register(HostingCell<Text>.self, forCellReuseIdentifier: "HostingCell<Text>")
         self.tableView.separatorStyle = .none
     }
 
@@ -28,23 +28,13 @@ final class TableViewController: UITableViewController {
         navigationItem.searchController = search
     }
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 300
+        300
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.row % 6 == 0 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "HostingCell<ComplicatedCellView>", for: indexPath) as! HostingCell<ComplicatedCellView>
-            cell.set(rootView: ComplicatedCellView(value: indexPath.row), parentController: self)
-            return cell
-        } else {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "HostingCell<CellView>", for: indexPath) as! HostingCell<CellView>
-            cell.set(rootView: CellView(content: "Title Title Title ", numberOfRepetitions: indexPath.row % 20 + 1), parentController: self)
-            return cell
-        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HostingCell<Text>", for: indexPath) as! HostingCell<Text>
+        cell.set(rootView: Text("Hi"), parentController: self)
+        return cell
     }
 }
